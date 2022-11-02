006371dc17a0bf3613f202630923365364196dfa
unknown <eng.baraahmassri@gmail.com>
2022-11-02 12:34:39 +0200

    Adding a Cart model

---
+++ models/cart.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const fs = require("fs");
+const p = path.join(path.dirname(require.main.filename), "data", "cart.json");
+
+module.exports = class Cart {
+  static addProduct(id, productPrice) {
+    fs.readFile(p, (err, fileContent) => {
+      let cart = { products: [], totalPrice: 0 };
+      if (!err) {
+        cart = JSON.parse(fileContent);
+      }
+      // Analyze the cart => Find existing product
+      const exsitingProductIndex = cart.products.findIndex(
+        (prod) => prod.id === id
+      );
+      const exsitingProduct = cart.products[exsitingProductIndex];
+      let updatedProduct;
+      if (exsitingProduct) {
+        updatedProduct = { ...exsitingProduct };
+        updatedProduct.qty = updatedProduct.qty + 1;
+        cart.products = [...cart.products];
+        cart.products[exsitingProductIndex] = updatedProduct;
+      } else {
+        updatedProduct = { id: id, qty: 1 };
+        cart.products = [...cart.products, updatedProduct];
+      }
+      cart.totalPrice = cart.totalPrice + +productPrice;
+      fs.writeFile(p, JSON.stringify(cart), (err) => {
+        console.log(err);
+      });
+    });
+  } //end addproduct
+};
