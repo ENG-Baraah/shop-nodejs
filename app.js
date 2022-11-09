eee5b0639cfaa72f5599c8be4d17fa63659fb5e0
unknown <eng.baraahmassri@gmail.com>
2022-11-09 12:38:52 +0200

    Using Sql Database with nodejs

--- app.js
+++ app.js
@@ -6,12 +6,17 @@ const bodyParser = require("body-parser");
 const app = express();
 
 const errorController = require("./controllers/error");
+const db = require("./util/database");
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+
+// db.execute("SELECT * FROM products")
+//   .then((result) => console.log(result[1]))
+//   .catch((err) => console.log(err));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
