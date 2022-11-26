1509f2034870ec47b4f2c877a70fc5a9193b5ca1
unknown <eng.baraahmassri@gmail.com>
2022-11-26 23:37:55 +0300

    Deleting Cart Items

--- controllers/shop.js
+++ controllers/shop.js
@@ -58,22 +58,15 @@ exports.getIndex = (req, res, next) => {
 //     });
 // };
 
-// exports.postCartDeleteItem = (req, res, next) => {
-//   const prodId = req.body.productId;
-//   req.user
-//     .getCart()
-//     .then((cart) => {
-//       return cart.getProducts({ where: { id: prodId } });
-//     })
-//     .then((products) => {
-//       const product = products[0];
-//       return product.cartItem.destroy();
-//     })
-//     .then((result) => {
-//       res.redirect("/cart");
-//     })
-//     .catch((err) => console.log(err));
-// };
+exports.postCartDeleteItem = (req, res, next) => {
+  const prodId = req.body.productId;
+  req.user
+    .deleteItemFromCart(prodId)
+    .then((result) => {
+      res.redirect("/cart");
+    })
+    .catch((err) => console.log(err));
+};
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
