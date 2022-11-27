c18a62a0cb163522081bf78a107c826b65f51d58
unknown <eng.baraahmassri@gmail.com>
2022-11-27 17:17:05 +0300

    Getting orders

--- controllers/shop.js
+++ controllers/shop.js
@@ -87,20 +87,20 @@ exports.postCart = (req, res, next) => {
     });
 };
 
-// exports.getOrders = (req, res, next) => {
-//   req.user
-//     .getOrders({ include: ["products"] })
-//     .then((orders) => {
-//       res.render("shop/orders", {
-//         pageTitle: "Your Orders",
-//         path: "/orders",
-//         orders: orders,
-//       });
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//     });
-// };
+exports.getOrders = (req, res, next) => {
+  req.user
+    .getOrders()
+    .then((orders) => {
+      res.render("shop/orders", {
+        pageTitle: "Your Orders",
+        path: "/orders",
+        orders: orders,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
 // exports.getCheckout = (req, res, next) => {
 //   res.render("shop/checkout", { pageTitle: "Checkout", path: "/checkout" });
--- models/user.js
+++ models/user.js
@@ -78,9 +78,17 @@ class User {
 
   addOrder() {
     const db = getDb();
-    return db
-      .collection("orders")
-      .insertOne(this.cart)
+    return this.getCart()
+      .then((products) => {
+        const order = {
+          items: products,
+          user: {
+            _id: new ObjectId(this._id),
+            name: this.name,
+          },
+        };
+        return db.collection("orders").insertOne(order);
+      })
       .then((result) => {
         this.cart = { items: [] };
         return db
@@ -89,8 +97,14 @@ class User {
             { _id: new ObjectId(this._id) },
             { $set: { cart: { items: [] } } }
           );
-      })
-      .catch((err) => console.log(err));
+      });
+  }
+  getOrders() {
+    const db = getDb();
+    return db
+      .collection("orders")
+      .find({ "user._id": new ObjectId(this._id) })
+      .toArray();
   }
 
   static findById(userId) {
