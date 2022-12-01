548ae22ac3c0c5178769a828eeafd029a00b1bc6
unknown <eng.baraahmassri@gmail.com>
2022-12-01 17:54:04 +0300

    Adding User Modell

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const errorController = require("./controllers/error");
 const mongoose = require("mongoose");
 const app = express();
-// const User = require("./models/user");
+const User = require("./models/user");
 
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -15,14 +15,14 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-// app.use((req, res, next) => {
-//   User.findById("637d3bc86f851d8297463e2d")
-//     .then((user) => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch((err) => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById("6388b7163880b5f1c4f97654")
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
@@ -34,8 +34,19 @@ mongoose
     "mongodb+srv://Baraah90:dXyq4TvdgGQUsk1L@cluster0.lhoodtp.mongodb.net/mongoose?retryWrites=true&w=majority"
   )
   .then((result) => {
+    User.findOne().then((user) => {
+      if (!user) {
+        const user = new User({
+          name: "Baraah",
+          email: "Baraah@gmail.com",
+          cart: { items: [] },
+        });
+        user.save();
+      }
+    });
     app.listen(3000);
   })
+
   .catch((err) => {
     console.log(err);
   });
